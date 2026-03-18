c2b7864261f7ebc06317d48b069d6d571cb79935
Hirusha Yohan <hirushayohanperera123@gmail.com>
2026-03-18 23:04:52 +0530

    feat(application): add CreateTask validator using FluentValidation

---
+++ TaskManagementAPI/TaskManagementApi.Application/Validators/CreateTaskValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System.Security.Cryptography.X509Certificates;
+using TaskManagementApi.Application.DTOs;
+
+namespace TaskManagementApi.Application.Validators
+{
+    public class CreateTaskValidator : AbstractValidator<CreateTaskRequest>
+    {
+        public CreateTaskValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Descrption)
+                .MaximumLength(500);
+
+            RuleFor(x => x.DueDate)
+                .GreaterThan(DateTime.UtcNow);
+        }
+    }
+}
